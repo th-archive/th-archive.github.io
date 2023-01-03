7e5b393cdae6dc911a56330f0c0a05317e2a191f
Tiffany Huang <goldentogawhale1258@gmail.com>
2023-01-03 15:46:56 -0800

    updating for 2022

--- 2022/reading-scripts.js
+++ 2022/reading-scripts.js
@@ -7,3 +7,18 @@ function hide(num) {
 	var img = document.getElementById(num);
 	img.classList.remove("visible");
 }
+
+function toggle(pageNum) {
+	var mains = document.getElementsByTagName("main");
+	var pseudoMains = document.getElementsByClassName("pseudo-main");
+	var toggles = document.getElementsByClassName("toggle");
+	for (var i = 0; i < mains.length; i++) {
+		mains[i].classList.remove("active");
+		pseudoMains[i].classList.remove("active");
+		toggles[i].classList.remove("active");
+	}
+	var pageNums = document.getElementsByClassName(pageNum);
+	for (var j = 0; j < pageNums.length; j++) {
+		pageNums[j].classList.add("active");
+	}
+}
