08d9da2ec2f44df0747b0392a3ebfca0b0a1ede5
Tiffany Huang <goldentogawhale1258@gmail.com>
2025-07-31 23:33:26 -0700

    feat(2025): start

---
+++ 2025/reading-scripts.js
@@ -0,0 +1,15 @@
+const main = document.querySelector("main");
+const columnRevs = document.querySelectorAll(".column-reverse");
+const columnH2 = document.querySelectorAll(".column-reverse h2");
+const columnImg = document.querySelectorAll(".column-reverse img");
+const columnRevHeight = columnRevs[0].clientHeight
+
+main.addEventListener("scroll", (event) => {
+		var offset = 2 * main.scrollTop;
+		columnRevs[0].style.transform = `translateY(${offset}px)`;
+		columnRevs[1].style.transform = `translateY(${offset}px)`;
+
+		if (main.scrollTop >= 893) {
+			main.scrollTo({top: 0})
+		}
+});
