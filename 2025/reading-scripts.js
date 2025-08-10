9b1a2b082745431bd8fbee9ee921fd1b3cde07c7
Tiffany Huang <goldentogawhale1258@gmail.com>
2025-08-10 15:10:00 -0700

    feat(reading): finish formatting

--- 2025/reading-scripts.js
+++ 2025/reading-scripts.js
@@ -1,5 +1,6 @@
 const imgs = document.querySelectorAll(".img-column img");
 const numbers = document.querySelectorAll(".number-column p");
+const infos = document.querySelectorAll(".info-column .info");
 const anglesBtwn = 360 / imgs.length;
 imgs.forEach((img, key) => {
   img.style.setProperty("--rotation", `${key * anglesBtwn}deg`);
@@ -7,4 +8,15 @@ imgs.forEach((img, key) => {
 });
 numbers.forEach((num, key) => {
   num.style.setProperty("--index", `${key}`);
+});
+infos.forEach((info, key) => {
+  info.style.setProperty("--index", `${key}`);
+});
+
+const main = document.querySelector("main");
+main.addEventListener("scroll", (event) => {
+  console.log(main.scrollLeft);
+  if (main.scrollLeft >= 4031) {
+    main.scrollTo({ left: 0 });
+  }
 });
