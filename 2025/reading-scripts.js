fcec9407236bff395a4f541cf37dff95457555ba
Tiffany Huang <goldentogawhale1258@gmail.com>
2025-08-10 00:31:30 -0700

    feat(reading): add imgs and number scrolling

--- 2025/reading-scripts.js
+++ 2025/reading-scripts.js
@@ -1,8 +1,10 @@
-const numberColumnH2 = document.querySelectorAll(".number-column h2");
-const anglesBtwn = 360 / numberColumnH2.length;
-numberColumnH2.forEach((h2) => {
-  const index = Number(h2.innerHTML);
-  h2.style.transform = `rotateY(${
-    index * anglesBtwn
-  }deg) translateZ(35vw) rotateY(90deg) `;
+const imgs = document.querySelectorAll(".img-column img");
+const numbers = document.querySelectorAll(".number-column p");
+const anglesBtwn = 360 / imgs.length;
+imgs.forEach((img, key) => {
+  img.style.setProperty("--rotation", `${key * anglesBtwn}deg`);
+  img.style.setProperty("--index", `${key}`);
+});
+numbers.forEach((num, key) => {
+  num.style.setProperty("--index", `${key}`);
 });
