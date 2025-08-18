1abe0b3eefbf6e8ea786179190bb17909700ae81
Tiffany Huang <goldentogawhale1258@gmail.com>
2025-08-17 20:08:03 -0700

    feat(2025): add reading

--- 2025/reading-scripts.js
+++ 2025/reading-scripts.js
@@ -14,9 +14,11 @@ infos.forEach((info, key) => {
 });
 
 const main = document.querySelector("main");
+const scrollableDiv = document.querySelector(".scrollable div");
 main.addEventListener("scroll", (event) => {
   console.log(main.scrollLeft);
-  if (main.scrollLeft >= 4031) {
+
+  if (main.scrollLeft >= scrollableDiv.clientWidth * (imgs.length - 0.1)) {
     main.scrollTo({ left: 0 });
   }
 });
